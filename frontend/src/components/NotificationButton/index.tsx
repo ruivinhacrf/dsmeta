7dc748e9e54b8c1ec2e070d3679b59c4a1041f1c
Ester Miriam <emvfg2002@gmail.com>
2022-11-20 23:27:39 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -2,6 +2,7 @@ import icon from "../../assets/img/notification-icon.svg"
 import { BASE_URL } from "../../utils/request";
 import axios from 'axios';
 import './styles.css'
+import { toast } from "react-toastify";
 
 type Props = {
 
@@ -12,7 +13,7 @@ type Props = {
 function handleClick(id:number){
 axios (`${BASE_URL}/sales/${id}/notification`)
 .then(response => {
-    console.log("SUCESSO");
+    toast.info("SMS enviado com sucesso");
 })
 
 }
